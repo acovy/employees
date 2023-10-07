d5d8a5c5687e6886c4115f56c339133487792b6c
Andrey <aseminmma@gmail.com>
2023-10-07 20:24:05 +0300

    add new element INCREASE

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,32 @@
-import "./app.css";
-import AppInfo from "../app-info/app-info";
-import SearchPanel from "../search-panel/search-panel"
-import AppFilter from "../app-filter/app-filter";
-import EmployeesList from "../employees-list/employees-list";
-import EmployeesAddForm from "../employees-add-form/employees-add-form";
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'John C.', salary: 800, increase: false},
+        {name: 'Alex M.', salary: 3000, increase: true},
+        {name: 'Carl W.', salary: 5000, increase: true},
+    ];
+
     return (
         <div className="app">
-            <AppInfo/>
+            <AppInfo />
 
             <div className="search-panel">
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-            <EmployeesList/>
+            
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
-    )
+    );
 }
 
 export default App;
